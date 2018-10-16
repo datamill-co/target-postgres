6faf129b4fa1fdec7073b69de0cba54a2e9c9755
Kevin Sanchez <kevin.sanchez@revolutionprep.com>
2018-10-16 13:36:05 -0700

    add necessary requirements

--- setup.py
+++ setup.py
@@ -1,5 +1,3 @@
-#!/usr/bin/env python
-
 from setuptools import setup, find_packages
 
 setup(
@@ -7,12 +5,14 @@ setup(
     version="0.0.1",
     description='Singer.io targets for loading data into SQL databases',
     classifiers=['Programming Language :: Python :: 3 :: Only'],
-    py_modules=['target_postgres'],
+    py_modules=['target_postgres','target_redshift'],
     install_requires=[
         'jsonschema==2.6.0',
         'psycopg2==2.7.4',
+        'singer-python==5.0.12',
         'psycopg2-binary==2.7.4',
-        'singer-python==5.0.12'
+        'boto3',
+        'arrow==0.12.1'
     ],
     entry_points='''
       [console_scripts]
