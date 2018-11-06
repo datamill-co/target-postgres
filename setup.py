2e458f831b79f19e36d87e50587d3475a4160f52
Kevin Sanchez <kevin.sanchez@revolutionprep.com>
2018-11-05 16:06:28 -0800

    reorder requirements to alphabetical order

--- setup.py
+++ setup.py
@@ -7,12 +7,12 @@ setup(
     classifiers=['Programming Language :: Python :: 3 :: Only'],
     py_modules=['target_postgres','target_redshift'],
     install_requires=[
+        'arrow==0.12.1',
+        'boto3',
         'jsonschema==2.6.0',
         'psycopg2==2.7.4',
-        'singer-python==5.0.12',
         'psycopg2-binary==2.7.4',
-        'boto3',
-        'arrow==0.12.1'
+        'singer-python==5.0.12',
     ],
     entry_points='''
       [console_scripts]
