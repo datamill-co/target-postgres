119e53007095425bb5ed3be70265dcb291b6b4b4
Andrew Madonna <andrew.w.madonna@gmail.com>
2018-11-06 14:59:31 -0500

    Fix CLI entrypoint

--- target_postgres/__init__.py
+++ target_postgres/__init__.py
@@ -92,7 +92,7 @@ def line_handler(streams, target, max_batch_rows, max_batch_size, line):
 
 def send_usage_stats():
     try:
-        version = pkg_resources.get_distribution('target-csv').version
+        version = pkg_resources.get_distribution('target-postgres').version
         conn = http.client.HTTPConnection('collector.singer.io', timeout=10)
         conn.connect()
         params = {
@@ -106,12 +106,12 @@ def send_usage_stats():
         response = conn.getresponse()
         conn.close()
     except:
-        logger.debug('Collection request failed')
+        LOGGER.debug('Collection request failed')
 
 def main(config, input_stream=None):
     try:
         if not config.get('disable_collection', False):
-            logger.info('Sending version information to singer.io. ' +
+            LOGGER.info('Sending version information to singer.io. ' +
                         'To disable sending anonymous usage data, set ' +
                         'the config parameter "disable_collection" to true')
             threading.Thread(target=send_usage_stats).start()
@@ -150,11 +150,7 @@ def main(config, input_stream=None):
         LOGGER.critical(e)
         raise e
 
-if __name__ == "__main__":
-    try:
-        args = utils.parse_args(REQUIRED_CONFIG_KEYS)
+def cli():
+    args = utils.parse_args(REQUIRED_CONFIG_KEYS)
 
-        main(args.config)
-    except Exception as e:
-        LOGGER.critical(e)
-        raise e
+    main(args.config)
