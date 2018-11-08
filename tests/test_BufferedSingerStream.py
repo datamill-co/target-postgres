875d5b4fd26ff83fd634f529c57ccc7641c04b03
AlexanderMann <mannimal09@gmail.com>
2018-11-08 15:52:09 -0600

    Add Record - Validation: Additional specification
    Add in additional tests for `add_record`.

--- tests/test_BufferedSingerStream.py
+++ tests/test_BufferedSingerStream.py
@@ -16,6 +16,7 @@ def test_add_record_message():
                                          CATS_SCHEMA['schema'],
                                          CATS_SCHEMA['key_properties'])
     assert singer_stream.add_record_message(stream.generate_record_message()) is None
+    assert not singer_stream.peek_invalid_records()
 
 
 def test_add_record_message__invalid_record():
@@ -26,6 +27,9 @@ def test_add_record_message__invalid_record():
     with pytest.raises(Error):
         singer_stream.add_record_message(stream.generate_record_message())
 
+    assert singer_stream.peek_invalid_records()
+    assert singer_stream.count == 0
+
 
 def test_add_record_message__invalid_record__detection_off():
     stream = InvalidCatStream(10)
@@ -35,6 +39,9 @@ def test_add_record_message__invalid_record__detection_off():
                                          invalid_records_detect=False)
 
     singer_stream.add_record_message(stream.generate_record_message())
+
+    assert singer_stream.peek_invalid_records()
+    assert singer_stream.count == 0
 
 
 def test_add_record_message__invalid_record__cross_threshold():
@@ -50,3 +57,6 @@ def test_add_record_message__invalid_record__cross_threshold():
 
     with pytest.raises(Error):
         singer_stream.add_record_message(stream.generate_record_message())
+
+    assert singer_stream.peek_invalid_records()
+    assert singer_stream.count == 0
