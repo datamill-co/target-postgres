526ddcefabb4ebcf17e03881b61ed8e9d96eca91
Andrew Madonna <andrew.w.madonna@gmail.com>
2019-01-09 10:43:30 -0500

    Rename package for available pypi name

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@
 from setuptools import setup, find_packages
 
 setup(
-    name='target-postgres',
+    name='singer-target-postgres',
     version="0.1.0",
     description='Singer.io target for loading data into postgres',
     classifiers=['Programming Language :: Python :: 3 :: Only'],
