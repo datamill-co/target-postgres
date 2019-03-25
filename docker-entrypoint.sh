c9caad76b702505b5dc240f94bd6fc9705d4d409
AlexanderMann <mannimal09@gmail.com>
2019-03-25 11:15:56 -0500

    Housekeeping: Improving local development by using compose settings

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,4 +1,10 @@
 #!/usr/bin/env bash
 
+python -m venv venv--target-postgres
+source /code/venv--target-postgres/bin/activate
+
+pip install -e .
+
 echo -e "\n\nINFO: Dev environment ready."
+
 tail -f /dev/null
