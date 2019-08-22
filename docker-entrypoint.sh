1034b384cf4ab2ff00681dab97b2dbc8b7ba80a3
AlexanderMann <mannimal09@gmail.com>
2019-08-22 14:32:19 -0400

    Refactor: Place venvs underneath a single folder to make caching simple in CI

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
-python -m venv venv--target-postgres
-source /code/venv--target-postgres/bin/activate
+python -m venv venv/target-postgres
+source /code/venv/target-postgres/bin/activate
 
 pip install -e .[tests]
 
--- tests/migrations/scripts/install_schema_versions.sh
+++ tests/migrations/scripts/install_schema_versions.sh
@@ -1,12 +1,12 @@
 #!/usr/bin/env bash
 set -e -x
 
-python -m venv venv--target-postgres--schema0
-source venv--target-postgres--schema0/bin/activate
+python -m venv /code/venv/target-postgres--schema0
+source /code/venv/target-postgres--schema0/bin/activate
 pip install "singer-target-postgres==0.1.2"
 deactivate
 
-python -m venv venv--target-postgres--schema1
-source venv--target-postgres--schema1/bin/activate
+python -m venv /code/venv/target-postgres--schema1
+source /code/venv/target-postgres--schema1/bin/activate
 pip install "singer-target-postgres==0.1.9"
 deactivate
