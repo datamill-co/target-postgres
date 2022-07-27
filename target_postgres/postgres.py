ba9450bc285a500e22b11b4944336da65bde545e
Luis Atala <luisatala@preyhq.com>
2022-07-27 15:42:26 -0400

    test writelines

--- target_postgres/postgres.py
+++ target_postgres/postgres.py
@@ -568,7 +568,7 @@ class PostgresTarget(SQLInterface):
         #save temp rows
 
         with open("/tmp/{}".format(temp_table_name), 'w') as f:
-            f.writ(csv_rows.readall())
+            f.writelines(csv_rows)
         copy = sql.SQL('COPY {}.{} ({}) FROM STDIN WITH CSV NULL AS {}').format(
             sql.Identifier(self.postgres_schema),
             sql.Identifier(temp_table_name),
